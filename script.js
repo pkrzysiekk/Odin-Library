cfa3721c19961136f8925053751d800677555f56
pkrzysiekk <pkrzysiek24@gmail.com>
2024-08-09 15:45:43 +0200

    Add displaying books function and css file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myLibrary=[];
-
+const main=document.querySelector("main");
 function Book(title,author,numOfPages,isRead){
     this.title=title;
     this.author=author;
@@ -12,8 +12,25 @@ Book.prototype.info=function(){
   ${this.isRead ? 'already read':'not yet read'}`;
 }
 function addBookToLibrary(){
-  
+
 }
 
 theHobbit=new Book('The Hobbit','J.R.R. Tolkien','295',true);
-console.log(theHobbit.info());
+myLibrary.push(theHobbit);
+function displayBooks(){
+  let i=0;
+  myLibrary.forEach((book)=>{
+  let bookCard =document.createElement("div");
+  let bookSpan =document.createElement("span");
+  bookCard.classList.add('card');
+  bookCard.setAttribute("id",i);
+  i++;
+  bookSpan.textContent=book.info();
+  bookCard.appendChild(bookSpan);
+  main.appendChild(bookCard);
+  
+  })
+}
+
+displayBooks();
+
